f6e5e50aef7ee7237d676fdf15f491689be63607
Alexandre Cayla <alex.cayla@gmail.com>
2019-07-29 15:09:37 +0800

    stupid coaching rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'form', to: 'pages#form', as: :form
+  get 'answer', to: 'pages#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
